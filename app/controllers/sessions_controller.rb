79d6b526778d0843c9b35b7eb5513de5056bbded
satodd1967 <seth.todd@outlook.com>
2020-10-07 16:54:02 -0500

    built out the create method in the sessions controller with authentication. Updated the form in the sessions home for login to route to the /login route.  Added a signup link to the bottom of that page.  Added a flash message for incorrect login info.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,4 +8,15 @@ class SessionsController < ActionController::Base
         redirect_to root_path
     end
 
+    def create
+        user = User.find_by(email: params[:user][:email])
+        if user && user.authenticate(params[:user][:password])
+            session[:user_id] = user.id
+            redirect_to user_path(user)
+        else
+            flash[:message] = "Incorrect Login"
+            redirect_to root_path
+        end
+    end
+
 end

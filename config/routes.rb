06032e57ba77c98bd412f6f1793ba693862b6df4
satodd1967 <seth.todd@outlook.com>
2020-10-07 16:10:06 -0500

    Commented out the login path in the routes.rb file as I plan to have the login form on the home page at sessions#home.

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get '/signup' => 'users#new'
   post '/signup' => 'users#create'
 
-  get '/login' => 'sessions#new'
+  # get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
   

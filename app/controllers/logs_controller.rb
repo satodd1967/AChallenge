2f20a2c4ac4539cbdc834b5c3bba30063be14b18
satodd1967 <seth.todd@outlook.com>
2020-10-08 20:31:19 -0500

    added a show page to the logs view and added in code to see the log date and user id.  Added a show method to the logs controller.

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,2 +1,9 @@
 class LogsController < ApplicationController
+
+    def show
+        redirect_if_not_logged_in
+        @log = Log.find_by_id(params[:id])
+        redirect_to root_path if !@log  
+    end
+
 end

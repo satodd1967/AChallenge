caa26363c6a49bab7b491be4bbffe0f089a98073
satodd1967 <seth.todd@outlook.com>
2020-10-08 19:02:17 -0500

    Added a challenges section to the users show page.  Added some code  to show if the challenge is owned by the user.  Added a logs section to the users show page.  Figured out that the booleans are setup incorrectly in the seed data.  Also figured out that the logs structure is setup incorrectly in the seed data.  I have logs for each challenge when there should only be one set of logs and each challenge should pull them in based on date range.

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -1,2 +1,9 @@
 class ChallengesController < ApplicationController
+
+    def show
+        redirect_if_not_logged_in
+        @challenge = Challenge.find_by_id(params[:id])
+        redirect_to root_path if !@challenge
+    end
+
 end

5d75e804a06e0540428ef6c9a006ec18a8b455e6
satodd1967 <seth.todd@outlook.com>
2020-10-11 17:31:11 -0500

    updated the home method in the  sessions controller to explicitly render home if a user is not logged in.  Updated a break in the bottom of the home form.  Added a create method to the challenge_goals controller.

--- app/controllers/challenge_goals_controller.rb
+++ app/controllers/challenge_goals_controller.rb
@@ -3,7 +3,14 @@ class ChallengeGoalsController < ApplicationController
     def new
         if params[:challenge_id] && @challenge = Challenge.find_by_id(params[:challenge_id])
             @challenge_goal = @challenge.challenge_goals.build(user_id: current_user.id)
+        else
+            redirect_to root_path 
         end
     end
+
+    def create
+        binding.pry
+    end
+
 
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,8 @@ class SessionsController < ApplicationController
     def home
         if  logged_in?
             redirect_to user_path(current_user)
+        else
+            render :home
         end
     end
 

d5643bc9fe41773b88560451cf248e3a22385678
satodd1967 <seth.todd@outlook.com>
2020-10-11 17:19:38 -0500

    built out the first part of the challenge_goals new method in the challenge goals controller.  Added the basic new challenge goal form to the new view for challenge goals.  Confirmed that the view has access to a challenge goal object that has both the current users user_id and the challenge id from which the user came from.

--- app/controllers/challenge_goals_controller.rb
+++ app/controllers/challenge_goals_controller.rb
@@ -2,7 +2,7 @@ class ChallengeGoalsController < ApplicationController
 
     def new
         if params[:challenge_id] && @challenge = Challenge.find_by_id(params[:challenge_id])
-            @challenge_goal = Challenges.challengegoals.build(@challenge)
+            @challenge_goal = @challenge.challenge_goals.build(user_id: current_user.id)
         end
     end
 

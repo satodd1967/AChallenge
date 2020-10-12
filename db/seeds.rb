70fadb7b3b577fde812ba3161def79db180bc91d
satodd1967 <seth.todd@outlook.com>
2020-10-12 10:58:35 -0500

    Updated the seed data again to account for the fact that log_scores that are created for a challenge_goal associated to a challenge and the log must be between the start and end date of the challenge.

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,18 +13,18 @@ patrick = User.create(email: "patrick@google.com", password: "patrick1", usernam
 joe = User.create(email: "joe@google.com", password: "joe1", username: "joe")
 
 # User Logs
-krisl1 = Log.create(log_date: "2020-20-05", worked_out: "1", tracked_food: "1", weight: "134.57", body_fat: ".22.75", active_calories: "600", calories: "2200", user: kris)
-krisl2 = Log.create(log_date: "2020-20-06", worked_out: "0", tracked_food: "0", weight: "133.57", body_fat: ".21.75", active_calories: "700", calories: "2300", user: kris)
-krisl3 = Log.create(log_date: "2020-20-07", worked_out: "0", tracked_food: "1", weight: "132.57", body_fat: ".20.75", active_calories: "800", calories: "2500", user: kris)
-timl1 = Log.create(log_date: "2020-20-05", worked_out: "1", tracked_food: "1", weight: "165.57", body_fat: ".25.75", active_calories: "300", calories: "3200", user: tim)
-timl2 = Log.create(log_date: "2020-20-06", worked_out: "0", tracked_food: "1", weight: "164.57", body_fat: ".25", active_calories: "400", calories: "3200", user: tim)
-timl3 = Log.create(log_date: "2020-20-07", worked_out: "1", tracked_food: "0", weight: "163.57", body_fat: ".24.75", active_calories: "500", calories: "3200", user: tim)
-patrickl1 = Log.create(log_date: "2020-20-05", worked_out: "1", tracked_food: "1", weight: "185.57", body_fat: ".29.75", active_calories: "600", calories: "3200", user: patrick)
-patrickl2 = Log.create(log_date: "2020-20-06", worked_out: "0", tracked_food: "1", weight: "184.57", body_fat: ".28.75", active_calories: "600", calories: "3200", user: patrick)
-patrickl3 = Log.create(log_date: "2020-20-07", worked_out: "1", tracked_food: "0", weight: "183.57", body_fat: ".27.75", active_calories: "600", calories: "3200", user: patrick)
-joel1 = Log.create(log_date: "2020-20-05", worked_out: "1", tracked_food: "1", weight: "185.57", body_fat: ".29.75", active_calories: "600", calories: "3200", user: joe)
-joel2 = Log.create(log_date: "2020-20-06", worked_out: "0", tracked_food: "1", weight: "184.57", body_fat: ".28.75", active_calories: "600", calories: "3200", user: joe)
-joel3 = Log.create(log_date: "2020-20-07", worked_out: "1", tracked_food: "1", weight: "183.57", body_fat: ".27.75", active_calories: "600", calories: "3200", user: joe)
+krisl1 = Log.create(log_date: "2020-10-05", worked_out: "1", tracked_food: "1", weight: "134.57", body_fat: ".22.75", active_calories: "600", calories: "2200", user: kris)
+krisl2 = Log.create(log_date: "2020-10-06", worked_out: "0", tracked_food: "0", weight: "133.57", body_fat: ".21.75", active_calories: "700", calories: "2300", user: kris)
+krisl3 = Log.create(log_date: "2020-10-07", worked_out: "0", tracked_food: "1", weight: "132.57", body_fat: ".20.75", active_calories: "800", calories: "2500", user: kris)
+timl1 = Log.create(log_date: "2020-10-05", worked_out: "1", tracked_food: "1", weight: "165.57", body_fat: ".25.75", active_calories: "300", calories: "3200", user: tim)
+timl2 = Log.create(log_date: "2020-10-06", worked_out: "0", tracked_food: "1", weight: "164.57", body_fat: ".25", active_calories: "400", calories: "3200", user: tim)
+timl3 = Log.create(log_date: "2020-10-07", worked_out: "1", tracked_food: "0", weight: "163.57", body_fat: ".24.75", active_calories: "500", calories: "3200", user: tim)
+patrickl1 = Log.create(log_date: "2020-10-05", worked_out: "1", tracked_food: "1", weight: "185.57", body_fat: ".29.75", active_calories: "600", calories: "3200", user: patrick)
+patrickl2 = Log.create(log_date: "2020-10-06", worked_out: "0", tracked_food: "1", weight: "184.57", body_fat: ".28.75", active_calories: "600", calories: "3200", user: patrick)
+patrickl3 = Log.create(log_date: "2020-10-07", worked_out: "1", tracked_food: "0", weight: "183.57", body_fat: ".27.75", active_calories: "600", calories: "3200", user: patrick)
+joel1 = Log.create(log_date: "2020-10-05", worked_out: "1", tracked_food: "1", weight: "185.57", body_fat: ".29.75", active_calories: "600", calories: "3200", user: joe)
+joel2 = Log.create(log_date: "2020-10-06", worked_out: "0", tracked_food: "1", weight: "184.57", body_fat: ".28.75", active_calories: "600", calories: "3200", user: joe)
+joel3 = Log.create(log_date: "2020-10-07", worked_out: "1", tracked_food: "1", weight: "183.57", body_fat: ".27.75", active_calories: "600", calories: "3200", user: joe)
 
 # Challenge 1
 c1 = Challenge.create(name: "challenge1", description: "Allot of stuff about Challenge1 and how its the best challenge ever", start_date: "2020-10-05", duration: "5", end_date: "2020-11-02", active_calorie_goal: "300", points_worked_out: "10", points_tracked_food: "10", points_met_calorie_goal: "10", points_maintain_weight: "10", points_maintain_body_fat: "10", points_met_active_calorie_goal: "10", user: kris)
@@ -42,53 +42,58 @@ c2 = Challenge.create(name: "challenge2", description: "Allot of stuff about cha
     patrickCg2 = ChallengeGoal.create(start_weight: "185.8", start_body_fat: ".2745", start_calorie_goal: "3100", user: patrick, challenge: c2)
 
 # Challenge 3
-c3 = Challenge.create(name: "challenge3", description: "Allot of stuff about challenge 3 and how its the 3rd best challenge ever", start_date: "2020-10-05", duration: "6", end_date: "2020-11-09", active_calorie_goal: "300", points_worked_out: "10", points_tracked_food: "10", points_met_calorie_goal: "10", points_maintain_weight: "10", points_maintain_body_fat: "10", points_met_active_calorie_goal: "10", user: tim)
+c3 = Challenge.create(name: "challenge3", description: "Allot of stuff about challenge 3 and how its the 3rd best challenge ever", start_date: "2020-10-15", duration: "6", end_date: "2020-11-24", active_calorie_goal: "300", points_worked_out: "10", points_tracked_food: "10", points_met_calorie_goal: "10", points_maintain_weight: "10", points_maintain_body_fat: "10", points_met_active_calorie_goal: "10", user: tim)
     # Challenge Goals
     krisCg3 = ChallengeGoal.create(start_weight: "135.5", start_body_fat: ".2323", start_calorie_goal: "2300", user: kris, challenge: c3)
     timCg3 = ChallengeGoal.create(start_weight: "175.2", start_body_fat: ".2545", start_calorie_goal: "3000", user: tim, challenge: c3)
 
 Log.all.each do |log|
     log.user.challenge_goals.each do |cg|
-        @ls = cg.log_scores.build
-        @ls.log_id = log.id
-        if @ls.log.worked_out == true
-            @ls.points_worked_out = @ls.challenge_goal.challenge.points_worked_out
-        else
-            @ls.points_worked_out = 0
+        if log.log_date >= cg.challenge.start_date && log.log_date <= cg.challenge.end_date
+            @ls = cg.log_scores.build
+            @ls.log_id = log.id
+            if @ls.log.worked_out == true
+                @ls.points_worked_out = @ls.challenge_goal.challenge.points_worked_out
+            else
+                @ls.points_worked_out = 0
+            end
+            if @ls.log.tracked_food == true
+                @ls.points_tracked_food = @ls.challenge_goal.challenge.points_tracked_food
+            else
+                @ls.points_tracked_food = 0
+            end
+            if @ls.log.calories <= @ls.challenge_goal.start_calorie_goal
+                @ls.points_met_calorie_goal = @ls.challenge_goal.challenge.points_met_calorie_goal
+            else
+                @ls.points_met_calorie_goal = 0
+            end
+            if @ls.log.weight <= @ls.challenge_goal.start_weight
+                @ls.points_maintain_weight = @ls.challenge_goal.challenge.points_maintain_weight
+            else
+                @ls.points_maintain_weight = 0
+            end
+            if @ls.log.body_fat <= @ls.challenge_goal.start_body_fat
+                @ls.points_maintain_body_fat = @ls.challenge_goal.challenge.points_maintain_body_fat
+            else
+                @ls.points_maintain_body_fat = 0
+            end
+            if @ls.log.active_calories >= @ls.challenge_goal.challenge.active_calorie_goal
+                @ls.points_met_active_calorie_goal = @ls.challenge_goal.challenge.points_met_active_calorie_goal
+            else
+                @ls.points_met_active_calorie_goal = 0
+            end
+            @ls.total_points = [
+                @ls.points_worked_out,
+                @ls.points_tracked_food,
+                @ls.points_met_calorie_goal,
+                @ls.points_maintain_weight,
+                @ls.points_maintain_body_fat,
+                @ls.points_met_active_calorie_goal
+            ].sum
+            @ls.save
         end
-        if @ls.log.tracked_food == true
-            @ls.points_tracked_food = @ls.challenge_goal.challenge.points_tracked_food
-        else
-            @ls.points_tracked_food = 0
-        end
-        if @ls.log.calories <= @ls.challenge_goal.start_calorie_goal
-            @ls.points_met_calorie_goal = @ls.challenge_goal.challenge.points_met_calorie_goal
-        else
-            @ls.points_met_calorie_goal = 0
-        end
-        if @ls.log.weight <= @ls.challenge_goal.start_weight
-            @ls.points_maintain_weight = @ls.challenge_goal.challenge.points_maintain_weight
-        else
-            @ls.points_maintain_weight = 0
-        end
-        if @ls.log.body_fat <= @ls.challenge_goal.start_body_fat
-            @ls.points_maintain_body_fat = @ls.challenge_goal.challenge.points_maintain_body_fat
-        else
-            @ls.points_maintain_body_fat = 0
-        end
-        if @ls.log.active_calories >= @ls.challenge_goal.challenge.active_calorie_goal
-            @ls.points_met_active_calorie_goal = @ls.challenge_goal.challenge.points_met_active_calorie_goal
-        else
-            @ls.points_met_active_calorie_goal = 0
-        end
-        @ls.total_points = [
-            @ls.points_worked_out,
-            @ls.points_tracked_food,
-            @ls.points_met_calorie_goal,
-            @ls.points_maintain_weight,
-            @ls.points_maintain_body_fat,
-            @ls.points_met_active_calorie_goal
-        ].sum
-        @ls.save
     end
 end
+
+
+

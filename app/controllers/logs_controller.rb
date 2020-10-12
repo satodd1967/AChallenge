e9ce44cbad70996d1489b98f0405a4edb509045a
satodd1967 <seth.todd@outlook.com>
2020-10-12 12:20:16 -0500

    started building out the Log new form.

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,9 +1,19 @@
 class LogsController < ApplicationController
 
+    def new
+        @log = Log.new
+    end
+
+    def create
+        binding.pry
+    end
+    
     def show
         redirect_if_not_logged_in
         @log = Log.find_by_id(params[:id])
         redirect_to root_path if !@log  
     end
+
+
 
 end

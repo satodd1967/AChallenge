2cc72118514cfc782ed163278acb3789174d0a2b
satodd1967 <seth.todd@outlook.com>
2020-10-11 20:05:20 -0500

    Updated the challenge model to have many log scores and updated the logs model to have many log scores.

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,8 +1,8 @@
 class Challenge < ApplicationRecord
   belongs_to :user
+  has_many :log_scores
   has_many :challenge_goals
   has_many :users, through: :challenge_goals
-
 
   def challenge_owner
     self.user

a85dd4f73780b2253f8ae1d7c132e3df1bbcb4a8
satodd1967 <seth.todd@outlook.com>
2020-10-12 20:18:32 -0500

    added a partial form into the logs view folder and moved the new form to that form and then added the render in the new view.

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -1,5 +1,5 @@
 class ChallengesController < ApplicationController
-
+    before_action :find_challenge, only: [:show, :edit, :update]
     before_action :redirect_if_not_logged_in    
 
     def index
@@ -24,10 +24,26 @@ class ChallengesController < ApplicationController
         redirect_to root_path if !@challenge
     end
 
+    def edit 
+    end
+
+    def update
+        @challenge.update(challenge_params)
+      if @challenge.save
+        redirect_to challenge_path(@challenge)
+      else
+        render :edit
+      end
+    end
+
     private
 
     def challenge_params
         params.require(:challenge).permit(:name, :description, :start_date, :duration, :end_date, :active_calorie_goal, :points_worked_out, :points_tracked_food, :points_met_calorie_goal, :points_maintain_weight, :points_maintain_body_fat, :points_met_active_calorie_goal)
+    end
+
+    def find_challenge
+        @challenge = Challenge.find_by(id: params[:id])
     end
 
 end

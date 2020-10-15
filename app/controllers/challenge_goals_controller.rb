8d86b97ba3f652e70d8892bc59e30428b3ff873c
satodd1967 <seth.todd@outlook.com>
2020-10-14 20:53:32 -0500

    Deleted some uneccessary code out of the logs controller associated to the before action methods.  Updated the ChallengeGoals controller with before action methods for find_challenge_goal and redirect if not logged in.  Also added the actual find challenge goal method to the private methods.

--- app/controllers/challenge_goals_controller.rb
+++ app/controllers/challenge_goals_controller.rb
@@ -1,4 +1,6 @@
 class ChallengeGoalsController < ApplicationController
+    before_action :find_challenge_goal, only: [:show, :edit, :update, :destroy]
+    before_action :redirect_if_not_logged_in 
 
     def new
         if params[:challenge_id] && @challenge = Challenge.find_by_id(params[:challenge_id])
@@ -18,7 +20,6 @@ class ChallengeGoalsController < ApplicationController
     end
 
     def show
-        binding.pry
     end
 
     def edit
@@ -33,6 +34,10 @@ class ChallengeGoalsController < ApplicationController
         params[:challenge_goal][:challenge_id] = params[:challenge_id]
         params[:challenge_goal][:start_body_fat] = to_float(params[:challenge_goal][:start_body_fat].to_f)
         params.require(:challenge_goal).permit(:start_weight, :start_body_fat, :start_calorie_goal, :challenge_id)
+    end
+
+    def find_challenge_goal
+        @challenge_goal = ChallengeGoal.find_by(id: params[:id])
     end
 
 
--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -18,7 +18,6 @@ class LogsController < ApplicationController
     end
     
     def show
-        redirect_if_not_logged_in
         @log = Log.find_by_id(params[:id])
         redirect_to root_path if !@log  
     end

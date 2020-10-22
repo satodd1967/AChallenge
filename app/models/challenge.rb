1d860c6e02debfb3b2834e4f76e45a2d92eea524
satodd1967 <seth.todd@outlook.com>
2020-10-21 22:53:42 -0500

    Added a method called logs to the challenge model that pulls all of the logs for that challenge through thhe log_scores.

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -2,10 +2,18 @@ class Challenge < ApplicationRecord
   belongs_to :user
   has_many :challenge_goals, :dependent => :destroy 
   has_many :users, through: :challenge_goals
+  has_many :logs, through: :users
   has_many :log_scores, through: :challenge_goals
 
   def challenge_owner
     self.user
   end
+
+  def logs
+    self.log_scores.each do |ls|
+      ls.log
+    end
+  end
+      
   
 end

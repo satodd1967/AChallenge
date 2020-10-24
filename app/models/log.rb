3a706a7a8a0828e6a1d772687608a9c9f2fc7f5d
satodd1967 <seth.todd@outlook.com>
2020-10-24 13:52:30 -0500

    Added a scope method to order logs descending on log_date to the logs model and then added that method in a chain on the users show page under Diary Logs.

--- app/models/log.rb
+++ app/models/log.rb
@@ -2,6 +2,8 @@ class Log < ApplicationRecord
   belongs_to :user
   has_many :log_scores, :dependent => :destroy
 
+  scope :last_first, -> { order(log_date: :desc) }
+
   def create_log_scores
     self.user.challenge_goals.each do |cg|
       if self.log_date >= cg.challenge.start_date && self.log_date <= cg.challenge.end_date

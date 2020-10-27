ed9a62986d7755838ae6c393a677483d714f9477
satodd1967 <seth.todd@outlook.com>
2020-10-26 20:56:38 -0500

    Added a define_attributes method int he application controller as a helper method.  This method can redifne the attributes of any object that is is passed and strip out unwanted attributes for the purpose of iterating over attributes instead of typing them all out.  Also added a reset attributes method in the same place.  The purpose of this method is to reset any of the attribute names for formating before they are titelized in the iteration.  Changed the logs show form fron an show/edit page to just a show page and wired up an edit button that takes you to the already built out edit page and added a delete button to that edit page.  Haven't tested validations for the logs since this was done.  Need to see if I can use these two methods to refactor the pages to allow for the use of partials in this format.  Tried it on the challenges show page and it did not work well as there was too much custom in the show components, however it can be used for the straight challenges piece I think.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
 
-    helper_method :current_user, :logged_in?, :to_percent, :to_float
+    helper_method :current_user, :logged_in?, :to_percent, :to_float, :define_attributes, :reset_attributes
 
 private
 
@@ -22,6 +22,24 @@ private
 
     def to_decimal(float)
         float/100
+    end
+
+    def define_attributes(object)
+        object.attributes.select {
+            |attr| attr != "id" &&
+             attr != "user_id" &&
+              attr != "updated_at" &&
+               attr != "created_at" &&
+                attr != "log_date"
+            }
+    end
+
+    def reset_attributes(attribute)
+        if attribute == "body_fat"
+            attributes = "body_fat %"
+        else
+            attribute
+        end
     end
 
 end

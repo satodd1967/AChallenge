3f28607c59f22baaee94ee505fd3d6733f37c8ed
satodd1967 <seth.todd@outlook.com>
2020-11-01 11:19:09 -0600

    Fixed spacing in the challenge_goals model.

--- app/models/challenge_goal.rb
+++ app/models/challenge_goal.rb
@@ -2,23 +2,23 @@ class ChallengeGoal < ApplicationRecord
 
     include Convert
 
-  belongs_to :user
-  belongs_to :challenge
-  has_many :log_scores, :dependent => :destroy
+    belongs_to :user
+    belongs_to :challenge
+    has_many :log_scores, :dependent => :destroy
 
-  before_save :convert_percent
+    before_save :convert_percent
 
-  validates_presence_of attribute_names.select {
-    |attr| attr != "id" &&
-     attr != "created_at" &&
-      attr != "updated_at" 
-    }
+    validates_presence_of attribute_names.select {
+      |attr| attr != "id" &&
+       attr != "created_at" &&
+        attr != "updated_at" 
+      }
   
-  validates_numericality_of attribute_names.select {
-    |attr| attr != "id" &&
-     attr != "created_at" &&
-      attr != "updated_at" 
-    }, greater_than: 0 
+    validates_numericality_of attribute_names.select {
+      |attr| attr != "id" &&
+       attr != "created_at" &&
+        attr != "updated_at" 
+      }, greater_than: 0 
 
     def cg_create_log_scores
       self.user.logs.each do |log|

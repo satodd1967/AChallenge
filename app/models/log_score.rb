9b719486977f1b7b89960e6e7b01c263ad8524fd
satodd1967 <seth.todd@outlook.com>
2020-11-01 19:59:17 -0600

    Added a log_show_attributes method to the log scores model.  Then used this method in an iteration in the challenges show page to show the log score attributes that are necessary for the view.

--- app/models/log_score.rb
+++ app/models/log_score.rb
@@ -1,4 +1,13 @@
 class LogScore < ApplicationRecord
   belongs_to :log
   belongs_to :challenge_goal
+
+  def log_show_attributes
+    self.log.attributes.select {|k, v| k != "id" &&
+     k != "created_at" &&
+      k != "updated_at" &&
+      k != "user_id"
+    }
+  end
+
 end

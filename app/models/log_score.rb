156964dcf1a48d928a19aef3657157d5d2db667c
satodd1967 <seth.todd@outlook.com>
2020-11-03 17:49:51 -0600

    Deleted the log_show attributes method out of the log scores class as it was no longer needed.

--- app/models/log_score.rb
+++ app/models/log_score.rb
@@ -2,12 +2,4 @@ class LogScore < ApplicationRecord
   belongs_to :log
   belongs_to :challenge_goal
 
-  def log_show_attributes
-    self.log.attributes.select {|k, v| k != "id" &&
-     k != "created_at" &&
-      k != "updated_at" &&
-      k != "user_id"
-    }
-  end
-
 end
